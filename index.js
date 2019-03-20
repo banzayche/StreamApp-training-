fae8a2c50e96a89900e841bedbbe47a1e925bbc1
Serhii Pro <serhiipro@Serhiis-iMac.local>
2019-03-20 09:12:06 +0200

    try very simple video stream

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hi there!')
+app.get('/catsstream', (req, res) => {
+  res.writeHead(200, {'Content-Type': 'video/mp4'});
+  const str = fs.createReadStream('cats.mp4');
+  str.pipe(res);
 });
 
 app.listen(3000, () => {
-  console.log('The server is started on http://localhost:3000');
+  console.log('The server was started on http://localhost:3000');
 })
